b5c2fd758739c2a73fbe4817f7818c4197637783
Pablo Ferreras <p.ferreras@talespin.company>
2023-03-21 17:04:29 +0100

    same

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == "__main__":
     # Run locally
-    uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
+    #uvicorn.run("app.api:app", host="0.0.0.0", port=8000, reload=True)
 
     # Run on Heroku
     port = int(os.environ.get('PORT', 8000))
